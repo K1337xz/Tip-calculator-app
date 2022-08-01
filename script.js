88c7893b10780b2f8f90a7d5032f9a0d4e523fd0
Koporson <108631492+Koporson@users.noreply.github.com>
2022-08-01 23:21:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const tip = document.querySelectorAll(".btn input");
-
+const bill = document.querySelector(".bill");
+const numberPl = document.querySelector(".numberppl");
+const tipTxt = document.querySelector(".tip");
+const error = document.querySelector(".errormsg");
 for (let i = 0; i < tip.length; i++) {
-	tip[i].addEventListener("click", calculateTip);
+	calculateTip(e);
 }
 
 function calculateTip(e) {
 	let num = this.id;
-	console.log(num / 100);
+	let precent = num / 100;
+	let billValue = bill.value;
+	let peopleValue = numberPl.value;
+	//tip
+	let tips = (precent * billValue) / peopleValue;
+	tipTxt.innerHTML = `$${tips}`;
+	//total
 }
+calculateTip();
